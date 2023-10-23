b3b0ca484ea043d498815bbd4dce12b9d1d02811
DanielRivasMD <danielrivasmd@gmail.com>
2023-10-22 21:07:00 -0600

    import remote

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package main
 
-import "hercules/cmd"
+import "github.com/DanielRivasMD/Hercules/cmd"
 
 func main() {
 	cmd.Execute()
