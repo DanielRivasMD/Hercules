286355d841c893dc13b3ae04e298a839713b1516
DanielRivasMD <danielrivasmd@gmail.com>
2023-11-13 14:23:04 -0600

    update root documentation

--- cmd/root.go
+++ cmd/root.go
@@ -21,8 +21,9 @@ import (
 	"github.com/spf13/cobra"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
+	"github.com/atrox/homedir"
 	"github.com/spf13/viper"
+	"github.com/ttacon/chalk"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -37,16 +38,21 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "hercules",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Web data bounty hunter",
+	Long: chalk.Green.Color("Daniel Rivas <danielrivasmd@gmail.com>") + `
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+` + chalk.Green.Color("Hercules") + chalk.Blue.Color(` will perform all the labours as commanded.
+
+
+`) + chalk.Green.Color("Hercules") + ` creates a convenient command line interphase
+with built-in and accessible documentation.
+`,
+
+	Example: `
+` + chalk.Cyan.Color("hercules") + ` help`,
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
