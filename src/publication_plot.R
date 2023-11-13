53bee1b143e6e1dcca74db932348e75d9b58bc49
DanielRivasMD <danielrivasmd@gmail.com>
2023-11-13 08:56:30 -0600

    add draft publicatin plot

--- src/publication_plot.R
+++ src/publication_plot.R
@@ -1,1 +1,21 @@
-# TODO: plot collected data
+
+df <- read_tsv('mauceli.raw', col_names = "authors")
+
+int <- 5
+up <- 40
+
+fr <- seq(0, up, by = int)
+
+tb <- df %>% pull(authors) %>% table
+
+
+for (f in fr) {
+  tb[which(tb >= f & tb < (f + int))] %>% print
+}
+
+dc <- tibble(authors = names(tb), count = tb)
+
+dc %<>% arrange(desc(count))
+
+write.csv(as.data.frame(dc), 'mauceli.csv', quote = F, row.names = F)
+
