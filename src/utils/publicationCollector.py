6380a81040529d14e99e89425913e5f434820a3f
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-06 20:35:25 -0600

    update util publication collector

--- src/utils/publicationCollector.py
+++ src/utils/publicationCollector.py
@@ -1,24 +1,25 @@
 ####################################################################################################
 
-def publication_collector():
+# TODO: extract affiliation
+def publication_collector(soup, csv_writer, authorQueried):
 
   # publication loop
-  for article in soup.find_all('div', class_="rslt"):
+  for article in soup.find_all('div', class_="docsum-content"):
     # title
-    title = article.find('p', class_="title").text
+    title = article.find('a', class_="docsum-title").text.replace('\n', '').replace('  ', '')
     # journal
-    journal = article.find('span', class_="jrnl").text
+    journal = article.find('span', class_="docsum-journal-citation short-journal-citation").text.replace('\n', '').replace('  ', '')
+    # pmid
+    pmid = article.find('span', class_='docsum-pmid').text.replace('\n', '').replace('  ', '')
     # authors
-    author_element = article.find('p', class_="desc")
-    authors = author_element.text
-    ind_authors = author_element.text.split(", ")
-    if re.match(authorQueried, ind_authors[0]):
-      author_position = "First"
-    elif re.match(authorQueried, ind_authors[-1]):
-      author_position = "Corresponding"
+    authors = article.find('span', class_='docsum-authors full-authors').text.split(', ')
+    if (authorQueried in authors[0]):
+      position = "First"
+    elif (authorQueried in authors[-1]):
+      position = "Corresponding"
     else:
-      author_position = "Middle"
+      position = "Middle"
     # write
-    csv_writer.writerow([title, journal, author_position, authors])
+    csv_writer.writerow([title, journal, position, authors])
 
 ####################################################################################################
