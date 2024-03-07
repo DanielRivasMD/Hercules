9a6eed65f94b03ff8ead92689be65dfe38bac0ea
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-07 10:16:21 -0600

    update using selenium web crawler bin pubmed

--- src/bin/pubmed.py
+++ src/bin/pubmed.py
@@ -15,6 +15,7 @@ from csv import writer
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 ####################################################################################################
@@ -27,16 +28,54 @@ from selenium.webdriver.common.keys import Keys
 # authorQueried = sys.argv[2]
 
 # arguments
-httpSource = 'https://pubmed.ncbi.nlm.nih.gov/?term=rivas-carrillo+sd%5Bauthor%5D&sort=date'
+httpSource = 'https://pubmed.ncbi.nlm.nih.gov/?term=rivas-carrillo+sd%5Bauthor%5D&sort=date&format=abstract&size=200'
 authorQueried = 'Rivas-Carrillo SD'
 
-# https://pubmed.ncbi.nlm.nih.gov/?term=rivas-carrillo+sd%5Bauthor%5D&sort=date
-
 # launch browser
-browser = webdriver.Chrome()
+browser = webdriver.Firefox()
 
 # directing to ncbi
 browser.get(httpSource)
+
+# get article number
+no = browser.find_element(By.CLASS_NAME, value='results-amount').find_element(By.CLASS_NAME, value='value').text
+
+# iterate on articles
+for artix in range(int(no)):
+  # adjust index
+  artix += 1
+  artix = str(artix)
+
+  # expand affiliations
+  browser.find_element(By.ID, value='search-result-1-' + artix + '-toggle-authors').click()
+  maxaff = 0
+
+  # select article information
+  article = browser.find_element(By.ID, value='search-result-1-' + artix + '-full-view-heading')
+
+  article.find_element(By.CLASS_NAME, value='heading-title').text
+  # 'Whole-genome comparison of endogenous retrovirus segregation across wild and domestic host species populations'
+
+  # iterate on authors
+  for author in article.find_elements(By.CLASS_NAME, value='authors-list-item'):
+    # collect author info
+    name = author.find_element(By.CLASS_NAME, value='full-name').text
+    print(name)
+    try:
+      # BUG: not finding elements
+      affs = author.find_elements(By.CLASS_NAME, value='affiliation-link')
+      for aff in range(affs):
+        affix = int(aff.text)
+      if (maxaff < affix):
+        maxaff = affix
+      print(affix)
+      print(maxaff)
+    except:
+      print(name + ' has no affiliation')
+
+  # WIP: split into number & text
+  aff = browser.find_element(By.ID, value='search-result-1-' + artix + '-full-view-affiliation-1').text.split('\n')
+  print(aff)
 
 ####################################################################################################
 
@@ -52,6 +91,7 @@ with open(dataDir + '/' + outputFile + '.csv', 'w') as csv_file:
 
   # load into bs4 from source
   soup = BeautifulSoup(source.text, 'html.parser')
+  .click()
   publication_collector(soup, csv_writer, authorQueried)
 
   # page loop
