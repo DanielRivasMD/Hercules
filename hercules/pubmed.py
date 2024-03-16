455c37ee4c424e940931158d4289c6e430e49fbe
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-16 04:03:20 -0600

    udpate pubmed

--- hercules/pubmed.py
+++ hercules/pubmed.py
@@ -1,10 +1,5 @@
 ####################################################################################################
 
-# load config
-from src.config.config import dataDir
-
-####################################################################################################
-
 # load dependencies
 import os
 import re
@@ -28,110 +23,107 @@ class Article:
   def __str__(self):
     return f'Article:\nTitle: {self.title}\nJournal: {self.journal}\nPMID: {self.pmid}\nAuthors: {self.authors}\nAffiliation: {self.affiliation}\n\nAbstract: {self.abstract}'
 
-# # arguments
-# httpSource = sys.argv[1]
-# authorQueried = sys.argv[2]
+def pubmed(author, outdir):
 
-# arguments
-httpSource = 'https://pubmed.ncbi.nlm.nih.gov/?term=rivas-carrillo+sd%5Bauthor%5D&sort=date&format=abstract&size=200'
-authorQueried = 'Rivas-Carrillo SD'
+  # arguments
+  httpSource = 'https://pubmed.ncbi.nlm.nih.gov/?term=' + author.replace(" ", "+") + '%5Bauthor%5D&sort=date&format=abstract&size=200'
 
-# launch browser
-browser = webdriver.Chrome()
-browser.maximize_window()
+  # launch browser
+  browser = webdriver.Chrome()
+  browser.maximize_window()
 
-# directing to ncbi
-browser.get(httpSource)
+  # directing to ncbi
+  browser.get(httpSource)
 
-# get article number
-no = browser.find_element(By.CLASS_NAME, value='results-amount').find_element(By.CLASS_NAME, value='value').text
+  # get article number
+  no = browser.find_element(By.CLASS_NAME, value='results-amount').find_element(By.CLASS_NAME, value='value').text
 
-# preallocate array
-articles = []
+  # preallocate array
+  articles = []
 
-# iterate on articles
-for artix in range(int(no)):
+  # iterate on articles
+  for artix in range(int(no)):
 
-  # instantiate article
-  article = Article()
+    # instantiate article
+    article = Article()
 
-  # adjust index
-  artix += 1
-  artix = str(artix)
+    # adjust index
+    artix += 1
+    artix = str(artix)
 
-  # expand affiliations
-  browser.find_element(By.ID, value='search-result-1-' + artix + '-toggle-authors').click()
-  maxaff = 0
+    # expand affiliations
+    browser.find_element(By.ID, value='search-result-1-' + artix + '-toggle-authors').click()
+    maxaff = 0
 
-  # select header information
-  artelem = browser.find_element(By.ID, value='search-result-1-' + artix + '-full-view-heading')
+    # select header information
+    artelem = browser.find_element(By.ID, value='search-result-1-' + artix + '-full-view-heading')
 
-  # assign journal
-  article.journal = artelem.find_element(By.ID, value='search-result-1-' + artix + '-full-view-journal-trigger').text
+    # assign journal
+    article.journal = artelem.find_element(By.ID, value='search-result-1-' + artix + '-full-view-journal-trigger').text
 
-  # assign title
-  article.title = artelem.find_element(By.CLASS_NAME, value='heading-title').text
+    # assign title
+    article.title = artelem.find_element(By.CLASS_NAME, value='heading-title').text
 
-  # assign pmid
-  article.pmid = artelem.find_element(By.CLASS_NAME, value='current-id').text
+    # assign pmid
+    article.pmid = artelem.find_element(By.CLASS_NAME, value='current-id').text
 
-  # iterate on authors
-  for author in artelem.find_elements(By.CLASS_NAME, value='authors-list-item'):
-    # collect author info
-    name = author.find_element(By.CLASS_NAME, value='full-name').text
-    affiliations = []
+    # iterate on authors
+    for aut in artelem.find_elements(By.CLASS_NAME, value='authors-list-item'):
+      # collect author info
+      name = aut.find_element(By.CLASS_NAME, value='full-name').text
+      affiliations = []
 
-    # collect affiliation info
-    try:
-      affs = author.find_elements(By.CLASS_NAME, value='affiliation-link')
-    except:
-      affs = []
-      print(name + ' has no affiliation')
+      # collect affiliation info
+      try:
+        affs = aut.find_elements(By.CLASS_NAME, value='affiliation-link')
+      except:
+        affs = []
+        print(name + ' has no affiliation')
 
-    if (len(affs) > 0):
-      for ax in range(len(affs)):
-        affix = int(affs[ax].text)
-        affiliations.append(affix)
-        if (maxaff < affix):
-          maxaff = affix
+      if (len(affs) > 0):
+        for ax in range(len(affs)):
+          affix = int(affs[ax].text)
+          affiliations.append(affix)
+          if (maxaff < affix):
+            maxaff = affix
 
-    # collect author affiliations
-    article.authors[name] = affiliations
+      # collect author affiliations
+      article.authors[name] = affiliations
 
-  # iterate on affiliations
-  for ax in range(maxaff):
-    ax += 1
-    # split into index & text
-    aff = browser.find_element(By.ID, value='search-result-1-' + artix + '-full-view-affiliation-' + str(ax)).text.split('\n')
-    if (len(aff) > 1):
-      article.affiliation[int(aff[0])] = aff[1]
+    # iterate on affiliations
+    for ax in range(maxaff):
+      ax += 1
+      # split into index & text
+      aff = browser.find_element(By.ID, value='search-result-1-' + artix + '-full-view-affiliation-' + str(ax)).text.split('\n')
+      if (len(aff) > 1):
+        article.affiliation[int(aff[0])] = aff[1]
 
-  # select body information
-  # assign abstract
-  article.abstract = browser.find_element(By.ID, value='search-result-1-' + artix + '-eng-abstract').text
+    # select body information
+    # assign abstract
+    article.abstract = browser.find_element(By.ID, value='search-result-1-' + artix + '-eng-abstract').text
 
-  # push article
-  articles.append(article)
+    # push article
+    articles.append(article)
 
-####################################################################################################
+  ####################################################################################################
 
-# prepare output file
-outputFile = authorQueried.replace(" ", "_")
-with open(dataDir + '/' + outputFile + '.csv', 'w') as csv_file:
-  csv_writer = writer(csv_file)
+  # prepare output file
+  outputFile = author.replace(" ", "_")
+  with open(outdir + '/' + outputFile + '.csv', 'w') as csv_file:
+    csv_writer = writer(csv_file)
 
-  # header
-  headers = ['Title', 'Journal', 'PMID', 'Authors']
-  csv_writer.writerow(headers)
+    # header
+    headers = ['Title', 'Journal', 'PMID', 'Authors']
+    csv_writer.writerow(headers)
 
-  # iterate on results
-  for article in articles:
-    row = [article.title, article.journal, article.pmid, ""]
-    csv_writer.writerow(row)
+    # iterate on results
+    for article in articles:
+      row = [article.title, article.journal, article.pmid, ""]
+      csv_writer.writerow(row)
 
-####################################################################################################
+  ####################################################################################################
 
-# close scrapper
-browser.quit()
+  # close scrapper
+  browser.quit()
 
-####################################################################################################
+  ####################################################################################################
