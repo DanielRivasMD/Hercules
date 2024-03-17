e4976e39314581195bfddf3c7b3caea0e9704b1b
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-17 14:05:57 -0600

    update schema sql

--- hercules/schema.sql
+++ hercules/schema.sql
@@ -17,21 +17,23 @@ CREATE TABLE Articles (
 
 -- authors table
 CREATE TABLE Authors (
+  author_pmid VARCHAR(255),
   author VARCHAR(255),
   pmid INT,
-  PRIMARY KEY (author),
+  PRIMARY KEY (author_pmid),
   FOREIGN KEY (pmid) REFERENCES `Articles`(pmid)
 );
 
 -- author affiliation table
 CREATE TABLE Affiliations (
   author_affiliation VARCHAR(255),
-  affilitation TEXT,
+  affiliation VARCHAR(255),
   author VARCHAR(255),
   pmid INT,
+  author_pmid VARCHAR(255),
   PRIMARY KEY (author_affiliation),
   FOREIGN KEY (pmid) REFERENCES `Articles`(pmid),
-  FOREIGN KEY (author) REFERENCES `Authors`(author)
+  FOREIGN KEY (author_pmid) REFERENCES `Authors`(author_pmid)
 );
 
 ----------------------------------------------------------------------------------------------------
