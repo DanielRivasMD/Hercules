83fdd02c8a24f30174edb82b9221b1943c2a2d04
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-17 13:20:08 -0600

    update & add insert articles connect sql

--- hercules/connect.py
+++ hercules/connect.py
@@ -5,15 +5,28 @@ from mysql.connector import connect
 
 ####################################################################################################
 
-with connect(
-  user='drivas',
-  password='hercules',
-  database='PubMed'
-) as connection:
-  s = 'SHOW DATABASES'
-  with connection.cursor() as cursor:
-    cursor.execute(s)
-    for result in cursor:
-      print(result)
+def insert_articles(articles, user='drivas', password='hercules', database='PubMed'):
+
+  insert_articles = """
+  INSERT INTO Articles
+  (pmid, journal, title, abstract)
+  VALUES (%s, %s, %s, %s)
+  """
+
+  ####################################################################################################
+
+  records_articles = []
+  for article in articles:
+    records_articles.append((article.pmid, article.journal, article.title, article.abstract))
+
+  with connect(
+    user=user,
+    password=password,
+    database=database
+  ) as connection:
+    with connection.cursor() as cursor:
+      cursor.executemany(insert_articles, records_articles)
+      connection.commit()
 
 ####################################################################################################
+
