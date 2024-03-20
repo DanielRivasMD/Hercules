51bc8458d0c957f0740b1fa5182deb70b9972d5b
DanielRivasMD <danielrivasmd@gmail.com>
2024-03-20 17:07:57 -0600

    add create database

--- hercules/connect.py
+++ hercules/connect.py
@@ -5,6 +5,61 @@ from mysql.connector import connect
 
 ####################################################################################################
 
+def create_db(database, user='drivas', password='hercules'):
+
+  sql_create_db = "CREATE DATABASE " + database + ";"
+  sql_use_db = "USE " + database + ";"
+
+  sql_create_articles = """
+  CREATE TABLE Articles (
+    pmid INT,
+    journal TEXT,
+    title TEXT,
+    abstract TEXT,
+    PRIMARY KEY (pmid)
+  );
+  """
+
+  sql_create_authors = """
+  CREATE TABLE Authors (
+    author_pmid VARCHAR(255),
+    author VARCHAR(255),
+    pmid INT,
+    PRIMARY KEY (author_pmid),
+    FOREIGN KEY (pmid) REFERENCES `Articles`(pmid)
+  );
+  """
+
+  sql_create_affiliations = """
+  CREATE TABLE Affiliations (
+    author_affiliation VARCHAR(255),
+    affiliation VARCHAR(255),
+    author VARCHAR(255),
+    pmid INT,
+    author_pmid VARCHAR(255),
+    PRIMARY KEY (author_affiliation),
+    FOREIGN KEY (pmid) REFERENCES `Articles`(pmid),
+    FOREIGN KEY (author_pmid) REFERENCES `Authors`(author_pmid)
+  );
+  """
+
+  ####################################################################################################
+
+  # WIP: look up how to create database
+  with connect(
+    user=user,
+    password=password
+  ) as connection:
+    with connection.cursor() as cursor:
+      cursor.execute(sql_create_db)
+      cursor.execute(sql_use_db)
+      cursor.execute(sql_create_articles)
+      cursor.execute(sql_create_authors)
+      cursor.execute(sql_create_affiliations)
+      connection.commit()
+
+####################################################################################################
+
 def insert_articles(articles, user='drivas', password='hercules', database='PubMed'):
 
   sql_insert_articles = """
